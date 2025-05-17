76f4222bbf26fa76b7bf40fdf24a394b6a3144d4
SKRTEEEEEE <adanreh.m@gmail.com>
2025-05-17 23:02:02 +0200

    refactor(src): unified res and errs

--- flows/domain.error.ts
+++ flows/domain.error.ts
@@ -20,43 +20,52 @@ export abstract class DomainError extends Error implements BaseFlow {
 }
 
 export class DatabaseActionError extends DomainError {
-    constructor(action: string, opt?:{entitie?: string, optionalMessage?: string}){
+    constructor(action: string, meta?:{entitie?: string, optionalMessage?: string}){
         super(
-            opt?.entitie?
-            `Action: ${action} in Database (for ${opt.entitie}) doesn't worked`:
+            meta?.entitie?
+            `Action: ${action} in Database (for ${meta.entitie}) doesn't worked`:
             `Action: ${action} in Database doesn't worked`,
             ErrorCodes.DATABASE_ACTION,
-            opt?.optionalMessage
+            meta?.optionalMessage
         )
     }
 }
 export class SetEnvError extends DomainError {
-    constructor(necessaryFor: string, opt?:{variable?: string, place?: string, optionalMessage?: string}){
+    constructor(necessaryFor: string, meta?:{variable?: string, place?: string, optionalMessage?: string}){
         super(
-            opt?.variable?
-            `EnvVariable: ${opt.variable} necessary for ${necessaryFor} (in ${opt.place ? opt.place : "[undefined]"}) doesn't set correctly`:
+            meta?.variable?
+            `EnvVariable: ${meta.variable} necessary for ${necessaryFor} (in ${meta.place ? meta.place : "[undefined]"}) doesn't set correctly`:
             `EnvVariable: necessary for ${necessaryFor} doesn't set correctly`,
             ErrorCodes.SET_ENV,
-            opt?.optionalMessage
+            meta?.optionalMessage
         )
     }
 }
 export class DatabaseFindError extends DomainError {
-    constructor(opt?: {entitie?: string, optionalMessage?: string}){
+    constructor(meta?: {entitie?: string, optionalMessage?: string}){
         super(
-            `Not found in database(${opt?.entitie})`,
+            `Not found in database(${meta?.entitie})`,
             ErrorCodes.DATABASE_FIND,
-            opt?.optionalMessage
+            meta?.optionalMessage
         )
     }
 }
 export class InputParseError extends DomainError {
-  constructor(message: string, opt?: { optionalMessage?: string; meta?: Record<string, any> }) {
+  constructor(message: string, meta?: { optionalMessage?: string;  }) {
     super(
       message,
       ErrorCodes.INPUT_PARSE,
-      opt?.optionalMessage,
-      opt?.meta
+      meta?.optionalMessage,
+      
+    );
+  }
+}
+export class UnauthorizedError extends DomainError {
+  constructor(message: string, meta?: { optionalMessage?: string;  }) {
+    super(
+      message,
+      ErrorCodes.UNAUTHORIZED_ACTION,
+      meta?.optionalMessage,
     );
   }
 }
