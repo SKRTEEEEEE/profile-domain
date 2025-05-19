e17bbd35021b666356b123a2e1661b8ce5126617
SKRTEEEEEE <adanreh.m@gmail.com>
2025-05-19 11:07:14 +0200

    refactor(user): better type

--- entities/user.d.ts
+++ entities/user.d.ts
@@ -13,5 +13,15 @@ type UserBase = UserFormS & {
     verifyTokenExpire?: string, // esto es para veificar el email
     nick?: string,
     paymentId?: string,
+} | {
+    address: string,
+    roleId: string | null,
+    role: RoleType | null,
+    solicitud: RoleType | null,
+    isVerified: boolean,
+    verifyToken?: string, // esto es para veificar el email
+    verifyTokenExpire?: string, // esto es para veificar el email
+    nick?: string,
+    paymentId?: string,
 }
 type User<TDBBase> = TDBBase & UserBase;
