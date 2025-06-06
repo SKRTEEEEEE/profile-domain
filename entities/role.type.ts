cfcc139fe94fea7519417eb68b524121efc2deee
SKRTEEEEEE <adanreh.m@gmail.com>
2025-06-07 00:09:24 +0200

    feat(role): add apiRoleType

--- entities/role.type.ts
+++ entities/role.type.ts
@@ -8,6 +8,14 @@ export enum RoleType {
     // PROF_PRO = "PROF_PRO"
   }
 
+  export const apiRoleType = {
+    enum: RoleType,
+    enumName: "RoleType",
+    description: "The Role permission type for a User",
+    title: "Role Type",
+    example: RoleType.ADMIN
+  }
+
   export const RoleHierarchy: Record<RoleType, number> = {
   [RoleType.STUDENT]: 3,
   [RoleType.STUDENT_PRO]: 2,
